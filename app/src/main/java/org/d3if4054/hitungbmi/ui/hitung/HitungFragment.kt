2ad07ce03d9153f1ba74754b1e3547f87fc2b657
kelvin_pradiza <kelvinpradiza@gmail.com>
2021-04-27 19:17:09 +0700

    Memperbaiki navigasi dengan view model

--- app/src/main/java/org/d3if4054/hitungbmi/ui/hitung/HitungFragment.kt
+++ app/src/main/java/org/d3if4054/hitungbmi/ui/hitung/HitungFragment.kt
@@ -1,4 +1,4 @@
-package org.d3if4054.hitungbmi.ui
+package org.d3if4054.hitungbmi.ui.hitung
 
 import android.content.Intent
 import android.os.Bundle
@@ -18,12 +18,12 @@ class HitungFragment : Fragment() {
 
     private val viewModel: HitungViewModel by viewModels()
     private lateinit var binding: FragmentHitungBinding
-    private lateinit var kategoriBmi: KategoriBmi
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentHitungBinding.inflate(layoutInflater, container, false)
         binding.button.setOnClickListener{ hitungBmi() }
-        binding.saranButton.setOnClickListener{ view: View -> view.findNavController().navigate(HitungFragmentDirections.actionHitungFragmentToSaranFragment(kategoriBmi))}
+
+        binding.saranButton.setOnClickListener{ viewModel.mulaiNavigasi() }
         binding.shareButton.setOnClickListener{ shareData() }
         setHasOptionsMenu(true)
         return binding.root
@@ -32,12 +32,10 @@ class HitungFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.getHasilBmi().observe(viewLifecycleOwner, {
+        viewModel.getNavigasi().observe(viewLifecycleOwner, {
             if (it == null) return@observe
-            binding.bmiTextView.text= getString(R.string.bmi_x, it.bmi)
-            binding.kategoriTextView.text = getString(R.string.kategori_x,
-                getKategori(it.kategori))
-            binding.buttonGroup.visibility = View.VISIBLE
+            findNavController().navigate(HitungFragmentDirections.actionHitungFragmentToSaranFragment(it))
+            viewModel.selesaiNavigasi()
         })
     }
 
